4a395cd98f6e0ce509eb11c0162868dd4f9844e6
alexwheeller <ak@aks-mac-mini.lan>
2015-12-06 11:01:59 +0000

    Initial Commit

---
+++ imgallery/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  imgallery
+//
+//  Created by AK on 06/12/2015.
+//  Copyright © 2015 AK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ imgallery/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  imgallery
+//
+//  Created by AK on 06/12/2015.
+//  Copyright © 2015 AK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
